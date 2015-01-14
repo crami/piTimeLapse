0837eab92a4b7430055439eed0331ac5ea7184e9
Matthias Cramer <matthias.cramer@iway.ch>
2015-01-14 17:15:57 +0100

    Button draw functions

--- pitimelapse.py
+++ pitimelapse.py
@@ -6,12 +6,42 @@
 import os
 import sys
 import pygame
+import time
 from pygame.locals import *
 from pygame.compat import unichr_, unicode_
 
 from itertools import chain
 
 SCREEN_SIZE = (320, 240)
+buttoncolor = 50,50,255
+
+
+def buttons(labels):
+  btn = pygame.Rect(0, 0, screen_rect.width/4 - 6, 20)
+
+  font = pygame.font.SysFont('ubuntu', 12)
+  
+  for i in range (0, 4):
+    btn.x=3+((screen_rect.width/4)*i)
+    btn.y=screen_rect.height-20
+    screen.fill(buttoncolor, btn)
+    label = font.render(labels[i], True, (255,255,255))
+    label_rect = label.get_rect()
+    label_rect.center = btn.center
+    screen.blit(label, label_rect)
+    
+def buttonpress(num,toggle):
+  width=(screen_rect.width/4)-6
+  height=20
+  x=3+((screen_rect.width/4)*i)
+  y=screen_rect.height-20
+  
+  if (toggle):
+    pygame.draw.polygon(screen, (255,255,255), [[x,y],[x+width,y],[x+width,y+height],[x,y+height]], 1)
+  else:
+    pygame.draw.polygon(screen, buttoncolor, [[x,y],[x+width,y],[x+width,y+height],[x,y+height]], 1)
+
+  pygame.display.flip()
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
@@ -34,17 +64,16 @@ label_rect.center = screen_rect.center
 """Blit image and text to the target surface."""
 screen.blit(label, label_rect)
 
-# Button areas
-
-btn = pygame.Rect(0, 0, screen_rect.width/4 - 6, 20)
-
-for i in range (0, 4):
-  btn.x=3+((screen_rect.width/4)*i)
-  btn.y=screen_rect.height-20
-  screen.fill(pygame.Color("red"), btn)
-  print(btn.center)
+btn_labels=['Button 1','Button 2','Button 3','Button 4']
+buttons(btn_labels)
 
 pygame.display.flip()
+
+for i in range (0, 4):
+  time.sleep(2)
+  buttonpress(i,1)
+  time.sleep(2)
+  buttonpress(i,0)
                             
 while(1):
   if pygame.event.wait().type in (QUIT, KEYDOWN, MOUSEBUTTONDOWN):
